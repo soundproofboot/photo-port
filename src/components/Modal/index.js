4477162cccd9311adf70bc5a12a8cc502e66dc5f
C Bares <colinedwinbares@gmail.com>
2022-04-03 21:29:32 -0500

    fixed toggle modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Modal = ({ currentPhoto }) => {
+const Modal = ({ onClose, currentPhoto }) => {
   const {name, category, description, index} = currentPhoto;
 
   return (
@@ -9,7 +9,7 @@ const Modal = ({ currentPhoto }) => {
         <h3 className="modalTitle">{name}</h3>
         <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
         <p>${description}</p>
-        <button type="button">Close this modal</button>
+        <button type="button" onClick={onClose}>Close this modal</button>
       </div>
     </div>
   );
